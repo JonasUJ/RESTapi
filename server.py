e8b1a918054644fa29759233c397d87ad388300a
JonasUJ <jonasuj@hotmail.dk>
2017-10-03 22:48:00 +0200

    -

--- server.py
+++ server.py
@@ -9,21 +9,31 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-argregex = re.compile(r"(\w+)=(\w+)")
+argregex = re.compile(r"([^&\s]+)=([^&\s]+)")
 
 class DataSplitter:
 
-    keywords = ['protocol', 'domain', 'tld', 'path']
+    keywords = {
+        'protocol': 'https',
+        'domain': '',
+        'tld': 'com',
+        'path': '',
+        'index': ''
+        }
 
     def __init__(self, data):
         self.params = dict()
         self.data = re.findall(argregex, data)
         for k, v in self.data:
-            if k in self.keywords:
+            if k in self.keywords.keys():
                 setattr(self, k, v)
             else:
                 self.params[k] = v
 
+        attrs = dir(self)
+        for k, v in self.keywords.items():
+            if not k in attrs:
+                setattr(self, k, v)
 
 
 class RespMeta(Resource):
@@ -38,11 +48,14 @@ class Resp(Resource):
         self.data = DataSplitter(data)
         print(self.data.params)
 
-        self.url = '{0.protocol}://www.{0.domain}.{0.tld}/{1}'.format(self.data, self.data.path.replace('-', '/'))
+        self.url = '{0.protocol}://www.{0.domain}.{0.tld}/{1}'.format(self.data, self.data.path.replace(',', '/'))
         print(self.url)
 
         with requests.get(self.url, params=self.data.params) as resp:
-            return resp.json()
+            extr = resp.json()
+            value = eval("extr" + self.data.index)
+            return value
+
 
  
 api.add_resource(Resp, '/request/<string:data>')
