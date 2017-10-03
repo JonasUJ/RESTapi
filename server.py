1347b49895d7da4c05c46061b3878b86ced315d4
JonasUJ <jonasuj@hotmail.dk>
2017-10-03 22:03:27 +0200

    -

--- server.py
+++ server.py
@@ -12,20 +12,30 @@ api = Api(app)
 argregex = re.compile(r"(\w+)=(\w+)")
 
 class DataSplitter:
+
+    keywords = ['url']
+
     def __init__(self, data):
+        self.params = dict()
         self.data = re.findall(argregex, data)
         for k, v in self.data:
-            setattr(self, k, v)
+            if k in self.keywords:
+                setattr(self, k, v)
+            else:
+                self.params[k] = v
 
 
 class RespMeta(Resource):
     def get(self):
         return 'My RESTful api, this is supposed to interact with the cleverbot.com RESTful api'
 
+
 class Resp(Resource):
     def get(self, data:DataSplitter):
-        #with requests.get(url)
-        return dir(data)
+        print(data, dir(data))
+        print(data.url, data.params)
+        #with requests.get(data.url, params=data.params) as resp:
+        return data
  
 api.add_resource(Resp, '/request/<string:url>')
 api.add_resource(RespMeta, '/meta')
