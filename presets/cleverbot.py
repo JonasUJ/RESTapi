e8908b259ad78465067b33fab8076bef3d89757e
JonasUJ <jonasuj@hotmail.dk>
2018-01-19 21:03:25 +0100

    remove quotes on response

--- presets/cleverbot.py
+++ presets/cleverbot.py
@@ -1,92 +1,78 @@
-import os
-import re
 import requests
-from urllib.parse import unquote
+import json
+from flask import Response, request
 
-from flask_restful import Resource
 from presets.cleverbotfool.fool import Fool
 
-global cs
-cs = ''
 
-def set_cs(conv):
-    global cs
-    cs = conv
+class CleverBot:
 
-def get_cs():
-    global cs
-    return cs
+    def __init__(self, user, key, cs=''):
+        self.user = user
+        self.key = key
+        self.url = 'https://www.cleverbot.com/getreply'
 
-ARG_RE = re.compile(r"([^&]+)=([^&]*)")
+        self.params = {
+            'key': key,
+            'cs': cs,
+            'callback': '',
+            'input': ''
+        }
 
-class DataSplitter:
+    def query(self, text, fool=False):
 
-    params = {
-        'input': '',
-        'key': '',
-        'cs': '',
-        'callback': '',
-        'fool': 'false'
-    }
-
-    def __init__(self, data):
-
-        self.data = re.findall(ARG_RE, data)
-
-        for k, v in self.data:
-            if k in self.params.keys():
-                self.params[k] = unquote(v)
-
-
-class Endpoint(Resource):
-
-    url = 'https://www.cleverbot.com/getreply'
-
-    def get(self, data):
-        self.data = DataSplitter(data)
-
-        inp = self.data.params['input']
-
-        fooled_entry = Fool.get(inp)
-        if self.data.params['fool'] == 'true' and fooled_entry:
-            print('In: {}\nFooled out: {}'.format(inp, fooled_entry))
+        fooled_entry = Fool.get(text)
+        if fool and fooled_entry:
+            print('In: {}\nFooled out: {}'.format(text, fooled_entry))
             return fooled_entry
 
-        if not self.data.params['cs']:
-            self.data.params['cs'] = get_cs()
-
-        if not self.data.params['key']:
-            return 'No cleverbot api key supplied'
+        self.params['input'] = text
 
         try:
-            with requests.get(
-                    self.url,
-                    params=self.data.params,
-                    timeout=8.0
-                ) as resp:
+            with requests.get(self.url, params=self.params, timeout=8.0) as resp:
+                respdict = resp.json()
 
-                response = resp.json()
-                print('Status:', response.get('status', 'n/a'))
-
-                if response.get('status', None) == '505':
+                if respdict.get('status') == '505':
                     return "I'm currently out of gas, but expecting a top up soon!"
-
-                try:
-                    output = response['output']
-                    set_cs(response['cs'])
-                except IndexError:
-                    return "Uh oh, looks like I didn't understand that"
-                except Exception as e:
-                    return str(e)
-
-        except requests.exceptions.Timeout as e:
+                elif respdict.get('status', 'success') == 'success':
+                    self.params['cs'] = respdict['cs']
+                    print('In: {}\nOut: {}'.format(respdict['input'], respdict['output']))
+                    return respdict['output']
+                else:
+                    print('Non-success status:', respdict['status'])
+                    return False
+        
+        except requests.exceptions.Timeout:
             return "Looks like something is broken, oops, expect me to be functional soon!"
         except Exception as e:
+            print(e)
             return 'Error, please contact us preferably with a screencap of your\'s and this message. [{}: {}]'.format(type(e), e)
 
-        print('In: {}\nOut: {}'.format(response['input'], response['output']))
-        return output
+
+class BotHandler:
+    
+    def __init__(self):
+        self.bots = dict()
 
 
-def setup(api, app, name, objects=None):
-    api.add_resource(Endpoint, '/{}/<string:data>'.format(name))
+    def get_bot(self, key='', user=''):
+        if self.bots.get(user):
+            return self.bots[user]
+        else:
+            print(f'Created new CleverBot for user: {user}')
+            self.bots[user] = CleverBot(user=user, key=key)
+            return self.bots[user]
+
+
+def setup(api, app, name, objects):
+
+    if not hasattr(objects, 'BotHandler'):
+        setattr(objects, 'BotHandler', BotHandler())
+
+    @app.route(f'/{name}')
+    def cleverbot_endpoint():
+        clevbot = objects.BotHandler.get_bot(key=request.args['key'], user=request.args.get('user', 'default'))
+        text = clevbot.query(request.args['input'])
+
+        resp = Response(text, status=200, mimetype='text/plain')
+        return resp
---
+++ presets/cleverbot_old.py
@@ -0,0 +1,92 @@
+import os
+import re
+import requests
+from urllib.parse import unquote
+
+from flask_restful import Resource
+from presets.cleverbotfool.fool import Fool
+
+global cs
+cs = ''
+
+def set_cs(conv):
+    global cs
+    cs = conv
+
+def get_cs():
+    global cs
+    return cs
+
+ARG_RE = re.compile(r"([^&]+)=([^&]*)")
+
+class DataSplitter:
+
+    params = {
+        'input': '',
+        'key': '',
+        'cs': '',
+        'callback': '',
+        'fool': 'false'
+    }
+
+    def __init__(self, data):
+
+        self.data = re.findall(ARG_RE, data)
+
+        for k, v in self.data:
+            if k in self.params.keys():
+                self.params[k] = unquote(v)
+
+
+class Endpoint(Resource):
+
+    url = 'https://www.cleverbot.com/getreply'
+
+    def get(self, data):
+        self.data = DataSplitter(data)
+
+        inp = self.data.params['input']
+
+        fooled_entry = Fool.get(inp)
+        if self.data.params['fool'] == 'true' and fooled_entry:
+            print('In: {}\nFooled out: {}'.format(inp, fooled_entry))
+            return fooled_entry
+
+        if not self.data.params['cs']:
+            self.data.params['cs'] = get_cs()
+
+        if not self.data.params['key']:
+            return 'No cleverbot api key supplied'
+
+        try:
+            with requests.get(
+                    self.url,
+                    params=self.data.params,
+                    timeout=8.0
+                ) as resp:
+
+                response = resp.json()
+                print('Status:', response.get('status', 'n/a'))
+
+                if response.get('status', None) == '505':
+                    return "I'm currently out of gas, but expecting a top up soon!"
+
+                try:
+                    output = response['output']
+                    set_cs(response['cs'])
+                except IndexError:
+                    return "Uh oh, looks like I didn't understand that"
+                except Exception as e:
+                    return str(e)
+
+        except requests.exceptions.Timeout as e:
+            return "Looks like something is broken, oops, expect me to be functional soon!"
+        except Exception as e:
+            return 'Error, please contact us preferably with a screencap of your\'s and this message. [{}: {}]'.format(type(e), e)
+
+        print('In: {}\nOut: {}'.format(response['input'], response['output']))
+        return output
+
+
+def setup(api, app, name, objects=None):
+    api.add_resource(Endpoint, '/{}/<string:data>'.format(name))
